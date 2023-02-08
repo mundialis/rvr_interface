d1d5aaf34f62c2d4a57dfd618db5604248b60288
Markus Metz <markus.metz.giswork@gmail.com>
2023-02-08 15:54:39 +0100

    ndom -> ndsm

--- grass-gis-addons/m.analyse.trees/r.trees.peaks/r.trees.peaks.py
+++ grass-gis-addons/m.analyse.trees/r.trees.peaks/r.trees.peaks.py
@@ -26,11 +26,11 @@
 # %end
 
 # %option G_OPT_R_INPUT
-# % key: ndom
+# % key: ndsm
 # % type: string
 # % required: yes
 # % multiple: no
-# % label: Name of the nDOM
+# % label: Name of the nDSM raster
 # %end
 
 # %option
@@ -62,7 +62,7 @@
 # % key: slope
 # % required: yes
 # % multiple: no
-# % label: Output raster map with peaks and ridges
+# % label: Output raster map with nDSM slope
 # % guisection: Output
 # %end
 
@@ -106,8 +106,8 @@ def main():
             "g.rename", raster="%s,%s" % ("MASK", tmp_mask_old), quiet=True
         )
 
-    ndom = options["ndom"]
-    ndom_slope = options["slope"]
+    ndsm = options["ndsm"]
+    ndsm_slope = options["slope"]
     nearest = options["nearest"]
     trees_peaks = options["peaks"]
     forms_res = float(options["forms_res"])
@@ -116,43 +116,43 @@ def main():
     org_region = grass.region()
     grass.use_temp_region()
 
-    # the resolution of the ndom must not be too fine,
+    # the resolution of the ndsm must not be too fine,
     # otherwise several peaks within a single tree crown will be detected
     # good choice: res=0.5 - 1
     grass.run_command("g.region", res=forms_res, flags="a")
-    ndom_resampled = ndom
-    rinfo = grass.raster_info(ndom)
+    ndsm_resampled = ndsm
+    rinfo = grass.raster_info(ndsm)
     if rinfo["nsres"] < forms_res:
-        ndom_resampled = f"{ndom}_resampled"
+        ndsm_resampled = f"{ndsm}_resampled"
         grass.run_command(
             "r.resamp.stats",
-            input=ndom,
-            output=ndom_resampled,
+            input=ndsm,
+            output=ndsm_resampled,
             method="maximum",
         )
     elif rinfo["nsres"] > forms_res:
-        ndom_resampled = f"{ndom}_resampled"
-        rm_rasters.append(ndom_resampled)
+        ndsm_resampled = f"{ndsm}_resampled"
+        rm_rasters.append(ndsm_resampled)
         radius_gauss = rinfo["nsres"] * 1.5
         radius_box = rinfo["nsres"] * 3
         grass.run_command(
             "r.resamp.filter",
-            input=ndom,
-            output=ndom_resampled,
+            input=ndsm,
+            output=ndsm_resampled,
             filter="gauss,box",
             radius=f"{radius_gauss},{radius_box}",
         )
 
-    ndom_forms = f"{ndom}_forms"
+    ndsm_forms = f"{ndsm}_forms"
     grass.run_command(
-        "r.geomorphon", elevation=ndom_resampled, forms=ndom_forms, search=7
+        "r.geomorphon", elevation=ndsm_resampled, forms=ndsm_forms, search=7
     )
-    rm_rasters.append(ndom_forms)
+    rm_rasters.append(ndsm_forms)
 
     # extract peak (2) + ridge (3)
     trees_peaks_tmp1 = f"{trees_peaks}_tmp1"
     grass.mapcalc(
-        f"{trees_peaks_tmp1} = if({ndom_forms} == 2 || {ndom_forms} == 3, 1, null())"
+        f"{trees_peaks_tmp1} = if({ndsm_forms} == 2 || {ndsm_forms} == 3, 1, null())"
     )
     rm_rasters.append(trees_peaks_tmp1)
 
@@ -168,7 +168,7 @@ def main():
     grass.run_command(
         "r.stats.zonal",
         base=trees_peaks_tmp1,
-        cover=ndom_forms,
+        cover=ndsm_forms,
         output=trees_peaks_tmp3,
         method="min",
     )
@@ -193,19 +193,19 @@ def main():
     # similar to watershed segmentation
     grass.run_command(
         "r.slope.aspect",
-        elevation=ndom,
-        slope=ndom_slope,
+        elevation=ndsm,
+        slope=ndsm_slope,
         format="percent",
         flags="e",
     )
     # invert slope, make sure all values are positive
-    grass.mapcalc(f"{ndom_slope}_inv = 10000 - {ndom_slope}")
-    rm_rasters.append(f"{ndom_slope}_inv")
+    grass.mapcalc(f"{ndsm_slope}_inv = 10000 - {ndsm_slope}")
+    rm_rasters.append(f"{ndsm_slope}_inv")
 
     # assign all pixels to the nearest (tree) peak
     grass.run_command(
         "r.cost",
-        input=f"{ndom_slope}_inv",
+        input=f"{ndsm_slope}_inv",
         output="trees_costs_tmp",
         nearest=nearest,
         start_raster=trees_peaks,
