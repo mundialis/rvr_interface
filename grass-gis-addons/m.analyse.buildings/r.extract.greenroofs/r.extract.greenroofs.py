e25ce8ea0b22736020609adf2018c856c1bf771a
Julia Haas <haas@mundialis.de>
2024-03-26 08:50:25 +0100

    fix typo

--- grass-gis-addons/m.analyse.buildings/r.extract.greenroofs/r.extract.greenroofs.py
+++ grass-gis-addons/m.analyse.buildings/r.extract.greenroofs/r.extract.greenroofs.py
@@ -447,7 +447,7 @@ def main():
     if options["used_thresh"] == "gb_perc":
         if not options["fnk_column"] or not options["fnk"]:
             grass.fatal(
-                _("If <gb_perc> is used <fnk> and <fnk_column> has to be set.")
+                _("If <gb_perc> is used <fnk> and <fnk_column> have to be set.")
             )
         gb_perc = options["gb_perc"]
         gb_thresh = calculate_gb_threshold(green_blue_ratio, fnk_vect, gb_perc)
